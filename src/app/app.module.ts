5d2f3f9adb3ba612bbccc1810c41345907e12853
comandoslinux <asistemas01@gmail.com>
2022-12-23 07:56:36 -0600

    I do not what a staged file is

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,16 +13,9 @@ import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
-import { LanguagesPage } from './pages/languages/languages.page';
-import { ConfessionsPage } from './pages/confessions/confessions.page';
-import { CategoriesPage } from './pages/categories/categories.page';
-import { ConfessionsnewPage } from './pages/confessionsnew/confessionsnew.page';
-import { ConfessionsshowPage } from './pages/confessionsshow/confessionsshow.page';
-
 @NgModule({
   declarations: [
-    AppComponent,
-    ConfessionsshowPage,
+    AppComponent
   ],
   entryComponents: [],
   imports: [
